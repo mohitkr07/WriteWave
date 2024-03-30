af6fa7b7b42c434ae2f13a3d07ef359596a633da
mohitkr07 <mo2002hit@gmail.com>
2024-03-30 21:12:12 +0530

    rebase main

--- src/assets/colors/Colors.js
+++
@@ -1,9 +0,0 @@
-export default Colors = {
-  PRIMARY: '#190482',
-  PRIMARY_LIGHT: '#7752FE',
-  LIGHT_PURPLE: '#8E8FFA',
-  TEXT: 'black',
-  THEME_TEXT: '#7E30E1',
-  WHITE: '#fff',
-  BG_THEME: '#F3F8FF',
-};
